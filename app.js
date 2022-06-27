9306945680a8753420fe00ccd18354a18c659ab6
Jordan D <degco44@gmail.com>
2022-06-26 21:32:14 -0500

    add filter and map to generating HTML

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 const inquirer = require('inquirer');
-/* const fs = require('fs');
+const fs = require('fs');
 const generatePage = require('./src/page-template.js');
-
-const pageHTML = generatePage(name, github);
-
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-}); */
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -129,7 +121,13 @@ const promptProject = portfolioData => {
 };
 
 promptUser()
-  .then(promptProject)
-  .then(portfolioData => {
-    console.log(portfolioData);
-  });
+    .then(promptProject)
+    .then(portfolioData => {
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
+
+            console.log('Portfolio complete! Check out index.html to see the output!');
+        });
+    });
